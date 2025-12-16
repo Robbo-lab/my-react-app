8622036a368fabbf7793936c89364b46402e6bb5
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-12-16 15:44:14 +0800

    chore: get the workflow to work with eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import "bulma/css/bulma.min.css";
 
 createRoot(document.getElementById("root")).render(<App />);
