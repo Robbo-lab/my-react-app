8ceead045cb5627315d88587ca536fe430e5492a
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-12-16 15:11:30 +0800

    feat: react 19.2 test & clean up

--- INSTALLATION.md
+++ INSTALLATION.md
@@ -13,7 +13,7 @@ cd my-react-app
 
 ### 2. **Initialise a New npm Project**
 
-Initialise a new npm project with:
+Initialise a new npm project with
 
 ```bash
 npm init -y. #or npm init for a manual
@@ -59,7 +59,8 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // cleans up the /dist folder before build
+    publicPath: "/",
+    clean: true,
   },
   module: {
     rules: [
@@ -78,11 +79,19 @@ module.exports = {
     }),
   ],
   devServer: {
+    port: 3000,
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "public"),
     },
     compress: true,
-    port: 3000,
+
+    // This is an example of how to set headers if you run into any issue with security in browser (i.e Tafe computers)
+    // headers: {
+    //   "Content-Security-Policy":
+    //     "default-src 'self'; style-src 'self' 'unsafe-inline' 'unsafe-eval'; script-src 'self' 'unsafe-eval'; connect-src 'self' http://localhost:3000 https://api.nasa.gov; img-src 'self' https://apod.nasa.gov;",
+    // },
+
+    historyApiFallback: true,
   },
 };
 ```
--- src/App.js
+++ src/App.js
@@ -11,55 +11,49 @@ import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/<repo>">
-      <Router>
-        <section className="hero is-fullheight is-primary">
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title">Our work in a SPA</h1>
-              <h2 className="subtitle">
-                All of our components in a Single Page React App
-              </h2>
-              <Link to="/">
-                <a className="button is-primary is-normal mr-1">Home </a>
-              </Link>
-              <Link to="/nasa-api">
-                <a className="button is-link is-normal mr-1">NASA API</a>
-              </Link>
-              <Link to="/styled">
-                <a className="button is-info is-normal mr-1">
-                  Styled Component
-                </a>
-              </Link>
-              <Link to="/accordion">
-                <a className="button is-success is-normal mr-1">Accordian</a>
-              </Link>
-              <Link to="/gallery">
-                <a className="button is-warning is-normal mr-1">Gallery</a>
-              </Link>
-              <Link to="/gallery/1">
-                <a className="button is-warning is-inverted is-normal mr-1">
-                  Gallery Item
-                </a>
-              </Link>
-              <Link to="/people">
-                <a className="button is-dark is-normal">People List</a>
-              </Link>
-              {/* Routing Setup */}
+    <Router>
+      <section className="hero is-fullheight is-primary">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title">Our work in a SPA</h1>
+            <h2 className="subtitle">
+              All of our components in a Single Page React App
+            </h2>
+            <Link className="button is-link is-normal m-1" to="/">
+              Home
+            </Link>
+            <Link className="button is-link is-normal m-1" to="/nasa-api">
+              NASA API
+            </Link>
+            <Link className="button is-link is-normal m-1" to="/styled">
+              Styled Component
+            </Link>
+            <Link className="button is-link is-normal m-1" to="/accordion">
+              Accordian
+            </Link>
+            <Link className="button is-link is-normal m-1" to="/gallery">
+              Gallery
+            </Link>
+            <Link className="button is-link is-normal m-1" to="/gallery/1">
+              Gallery Item
+            </Link>
+            <Link className="button is-link is-normal mr-1" to="/people">
+              People List
+            </Link>
 
-              <Routes>
-                <Route path="/nasa-api" element={<ApodPage />} />
-                <Route path="/styled" element={<StyledComponent />} />
-                <Route path="/accordion" element={<Accordion />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/gallery/:id" element={<GalleryItem />} />
-                <Route path="/people" element={<PeopleList />} />
-              </Routes>
-            </div>
+            {/* Routing Setup */}
+            <Routes>
+              <Route path="/nasa-api" element={<ApodPage />} />
+              <Route path="/styled" element={<StyledComponent />} />
+              <Route path="/accordion" element={<Accordion />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/gallery/:id" element={<GalleryItem />} />
+              <Route path="/people" element={<PeopleList />} />
+            </Routes>
           </div>
-        </section>
-      </Router>
-    </BrowserRouter>
+        </div>
+      </section>
+    </Router>
   );
 };
 
